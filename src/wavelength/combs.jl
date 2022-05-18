3dcbec80325b9df3fce63c7b54d2297d478323fd
Bryson Cale <bryson.cale1@gmail.com>
2022-05-18 09:46:32 -0700

    small fixes

--- src/wavelength/combs.jl
+++ src/wavelength/combs.jl
@@ -4,7 +4,7 @@ using Polynomials
 using Peaks
 using Infiltrator
 using PyPlot
-using ModelingToolkit, GalacticOptim, Optim
+using ModelingToolkit, GalacticOptim, Optim, GalacticOptimJL
 
 function estimate_peak_spacing(xi, xf, λi, λf, λ_estimate, ν0, Δν)
     integers, lfc_centers_λ_theoretical = gen_theoretical_peaks(λi, λf, ν0, Δν)
@@ -94,24 +94,32 @@ function get_peaks(λ_estimate, lfc_flux, ν0, Δν, xrange; σ_guess=[0.2, 1.4,
         yy ./= nanmaximum(yy)
         
         # System
-        @variables A μ σ B
-        @parameters x[1:length(xx)] y[1:length(xx)]
-        loss = begin
-            model = maths.gauss(collect(x), A, μ, σ) .+ B
-            sqrt(sum((collect(y) .- model).^2) / length(x))
+        # @variables A μ σ B
+        # @parameters x[1:length(xx)] y[1:length(xx)]
+        # loss = begin
+        #    model = maths.gauss(collect(x), A, μ, σ) .+ B
+        #    sqrt(sum((collect(y) .- model).^2) / length(x))
+        # end
+        # @named sys = OptimizationSystem(loss, [A, μ, σ, B], [x, y])
+        loss = (pars) -> begin
+            model = maths.gauss(xx, pars[1], pars[2], pars[3]) .+ pars[4]
+            return sqrt(sum((yy .- model).^2) / length(xx))
         end
-        @named sys = OptimizationSystem(loss, [A, μ, σ, B], [x, y])
 
         # Pars and bounds
-        u0 = [A => 1.0, μ => good_peaks[i], σ => σ_guess[2], B => 0.1]
-        lb = [A => 0.7, μ => good_peaks[i] + μ_bounds[1], σ => σ_guess[1], B => -0.5]
-        ub = [A => 1.3, μ => good_peaks[i] + μ_bounds[2], σ => σ_guess[3], B => 0.5]
-        p = [x => xx, y => yy]
+        # u0 = [A => 1.0, μ => good_peaks[i], σ => σ_guess[2], B => 0.1]
+        # lb = [A => 0.7, μ => good_peaks[i] + μ_bounds[1], σ => σ_guess[1], B => -0.5]
+        # ub = [A => 1.3, μ => good_peaks[i] + μ_bounds[2], σ => σ_guess[3], B => 0.5]
+        # p = [x => xx, y => yy]
+        
+        u0 = [1.0, good_peaks[i], σ_guess[2], 0.1]
 
-        prob = OptimizationProblem(sys, u0, p, grad=false, hess=false)
+        #prob = OptimizationProblem(sys, u0, p, grad=false, hess=false)
+        #prob = OptimizationProblem(loss, u0, grad=false, hess=false)
+        ubest = Optim.optimize(loss, u0, NelderMead()) |> Optim.minimizer
 
         # Fit
-        ubest = solve(prob, NelderMead(), maxiters=1000)
+        #ubest = solve(prob, NelderMead(), maxiters=1000)
 
         # Results
         amplitudes[i] = ubest[1]
@@ -121,9 +129,10 @@ function get_peaks(λ_estimate, lfc_flux, ν0, Δν, xrange; σ_guess=[0.2, 1.4,
         rms[i] = maths.rmsloss(maths.gauss(xx, amplitudes[i], lfc_centers_pix[i], σs[i]) .+ offsets[i], yy)
 
         #@infiltrate
+        #pygui(true)
         #begin
-        #    plot(xx, yy);
-        #    plot(xx, maths.gauss(xx, amplitudes[i], lfc_centers_pix[i], σs[i]) .+ offsets[i]);
+        #  plot(xx, yy);
+        #  plot(xx, maths.gauss(xx, amplitudes[i], lfc_centers_pix[i], σs[i]) .+ offsets[i]);
         #end
     end
 
--- src/wavelength/polywave_old.jl
+++ src/wavelength/polywave_old.jl
@@ -4,10 +4,7 @@ using EchelleBase
 using EchelleSpectralModeling
 
 using PyCall
-using LsqFit
-using Infiltrator
-using ForwardDiff
-using Flux
+using SciPy
 using Statistics
 
 export PolyλSolution
@@ -52,70 +49,56 @@ function EchelleSpectralModeling.get_init_parameters(m::PolyλSolution, data::Sp
         pars[pname] = Parameter(value=λ_zero_points[i], lower_bound=λ_zero_points[i] + m.bounds[1], upper_bound=λ_zero_points[i] + m.bounds[2])
     end
     return pars
+
 end
 
 ## 2d CC polynomial
-function build_λsolution_chebyval2d(chebs_pixels, chebs_orders, orders, nx, nm, coeffs)
+function build_λsolution_chebyval2d(pixels, orders, max_pixel, max_order, coeffs)
+    nx = length(pixels)
+    nm = length(orders)
     λ = fill(NaN, (nm, nx))
     for i=1:nx
         for m=1:nm
-            s = 0
-            for j=1:n
-                for k=1:m
-                    s += coeffs[j, k] * chebs_pixels[i] * chebs_orders[i] / orders[i]
-                end
-            end
-            λ[m, i] = s
+            order = orders[m]
+            pixel = pixels[i]
+            λ[m, i] = maths.chebyval2d(pixel / max_pixel, order / max_order, coeffs) / order
         end
     end
     return λ
 end
 
-function build_λsolution_chebyval2d_flat(chebs_pixels, chebs_orders, coeffs, orders)
-    nx = length(chebs_pixels)
-    λ = zeros(nx)
-    m, n = size(coeffs)
-    for i=1:nx
-        s = 0.0
-        for j=1:n
-            for k=1:m
-                s += coeffs[k, j] * chebs_pixels[i][j] * chebs_orders[i][k] / orders[i]
-            end
-        end
-        λ[i] = s
+function build_λsolution_chebyval2d_flat(pixels, orders, max_pixel, max_order, coeffs)
+    n = length(pixels)
+    λ = zeros(n)
+    for i=1:n
+        λ[i] = maths.chebyval2d(pixels[i] / max_pixel, orders[i] / max_order, coeffs) / orders[i]
     end
     return λ
 end
 
-function fit_peaks_cc2d(pixel_centers, orders, λ_centers, weights, max_pixel, max_order, nx, deg_inter_order, deg_intra_order, n_iterations=1)
+function fit_peaks_cc2d(pixel_centers, λ_centers, weights, orders, max_pixel, max_order, nx, deg_inter_order, deg_intra_order, n_iterations=1)
 
-    # Chebs
-    chebs_pixels, chebs_orders = maths.get_chebvals(pixel_centers, orders, max_pixel, max_order, deg_intra_order, deg_inter_order)
+    # Pars and bounds
+    loss = (coeffs, _weights) -> begin
+        #_coeffs = reshape(coeffs, (deg_inter_order+1, deg_intra_order+1))
+        numpy = pyimport("numpy")
+        model = build_λsolution_chebyval2d_flat(pixel_centers, orders, max_pixel, max_order, numpy.reshape(coeffs, (deg_inter_order+1, deg_intra_order+1)))
+        wres = _weights .* (λ_centers .- model)
+        return wres
+    end
 
-    # Initial params and weights
-    p0 = ones((deg_inter_order + 1) * (deg_intra_order + 1)) / 100
+    u0 = ones((deg_inter_order + 1) * (deg_intra_order + 1)) / 100
     weights_running = copy(weights)
-    coeffs_best = copy(p0)
-
-    # Load scipy
-    scipyopt = pyimport("scipy.optimize")
+    coeffs_best = copy(u0)
+    numpy = pyimport("numpy")
 
     for i=1:n_iterations
-        
-        # Loss
-        loss = (coeffs) -> begin
-            _model = build_λsolution_chebyval2d_flat(chebs_pixels, chebs_orders, reshape(coeffs, (deg_inter_order+1, deg_intra_order+1)), orders)
-            return weights_running .* (λ_centers .- _model)
-        end
-
-        # Lsq
-        result = scipyopt.least_squares(loss, p0, max_nfev=400 * length(coeffs_best), method="lm")
-        coeffs_best = result["x"]
-
-        # Flag
-        model_best = build_λsolution_chebyval2d_flat(chebs_pixels, chebs_orders, reshape(coeffs_best, (deg_inter_order+1, deg_intra_order+1)), orders)
-        residuals = maths.δλ2δv(λ_centers .- model_best, λ_centers)
-        bad = findall(abs.(residuals) .> 3 * maths.robust_σ(residuals))
+        result = SciPy.optimize.least_squares(loss, coeffs_best, method="lm", args=(weights_running,), max_nfev=200 * length(u0))
+        coeffs_best .= result["x"]
+        model_best = build_λsolution_chebyval2d_flat(pixel_centers, orders, max_pixel, max_order, numpy.reshape(coeffs_best, (deg_inter_order+1, deg_intra_order+1)))
+        residuals = λ_centers .- model_best
+        residuals .= maths.δλ2δv(residuals, model_best)
+        bad = findall(abs.(residuals) .> 3 * std(residuals))
         if length(bad) == 0
             break
         end
@@ -123,6 +106,6 @@ function fit_peaks_cc2d(pixel_centers, orders, λ_centers, weights, max_pixel, m
     end
 
     # Return
-    coeffs_best = reshape(coeffs_best, (deg_inter_order+1, deg_intra_order+1))
+    coeffs_best = numpy.reshape(coeffs_best, (deg_inter_order+1, deg_intra_order+1))
     return coeffs_best
 end
